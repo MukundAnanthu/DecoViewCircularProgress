e7a235014854ddae3679503c357f7ef5f751dd16
Mukund A <mukund.code@gmail.com>
2017-01-25 21:08:02 +0530

    Got a green dot

--- app/src/main/java/com/dwarka/mukund/drawingcircularprogressbars/MainActivity.java
+++ app/src/main/java/com/dwarka/mukund/drawingcircularprogressbars/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dwarka.mukund.drawingcircularprogressbars;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+
+import com.hookedonplay.decoviewlib.DecoView;
+import com.hookedonplay.decoviewlib.charts.SeriesItem;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DecoView arcView = (DecoView)findViewById(R.id.dynamicArcView);
+
+        // Create background track
+        arcView.addSeries(new SeriesItem.Builder(Color.argb(255, 218, 218, 218))
+                .setRange(0, 100, 100)
+                .setInitialVisibility(false)
+                .setLineWidth(32f)
+                .build());
+
+        //Create data series track
+        // The color set is the color of the arc representing progress
+        SeriesItem seriesItem1 = new SeriesItem.Builder(Color.argb(255, 64, 196, 0))
+                .setRange(0, 100, 0)
+                .setLineWidth(32f)
+                .build();
+
+        int series1Index = arcView.addSeries(seriesItem1);
     }
 }
