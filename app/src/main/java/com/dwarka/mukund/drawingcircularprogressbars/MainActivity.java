a7311ab5629dfb46eb5c0ab12cfa3b54034c6c24
Mukund A <mukund.code@gmail.com>
2017-01-25 22:35:30 +0530

    Got the progress bar to display different progress levels.

--- app/src/main/java/com/dwarka/mukund/drawingcircularprogressbars/MainActivity.java
+++ app/src/main/java/com/dwarka/mukund/drawingcircularprogressbars/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.hookedonplay.decoviewlib.DecoView;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
+import com.hookedonplay.decoviewlib.events.DecoEvent;
 
 public class MainActivity extends Activity {
 
@@ -31,5 +32,15 @@ public class MainActivity extends Activity {
                 .build();
 
         int series1Index = arcView.addSeries(seriesItem1);
+
+        // Add events to animate the data series
+        arcView.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
+                .setDelay(1000)
+                .setDuration(2000)
+                .build());
+
+        arcView.addEvent(new DecoEvent.Builder(25).setIndex(series1Index).setDelay(4000).build());
+        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series1Index).setDelay(8000).build());
+        arcView.addEvent(new DecoEvent.Builder(10).setIndex(series1Index).setDelay(12000).build());
     }
 }
