c6cdcf057771e40ecf949544d799c416db2fe873
Mukund A <mukund.code@gmail.com>
2017-01-26 12:49:11 +0530

    Got the Handler to increment the progress from 0 to 100 in increments of 1 unit per second

--- app/src/main/java/com/dwarka/mukund/drawingcircularprogressbars/MainActivity.java
+++ app/src/main/java/com/dwarka/mukund/drawingcircularprogressbars/MainActivity.java
@@ -3,6 +3,7 @@ package com.dwarka.mukund.drawingcircularprogressbars;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.hookedonplay.decoviewlib.DecoView;
 import com.hookedonplay.decoviewlib.charts.SeriesItem;
@@ -15,9 +16,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DecoView arcView = (DecoView)findViewById(R.id.dynamicArcView);
+        final DecoView arcView = (DecoView)findViewById(R.id.dynamicArcView);
 
         // Create background track
+        // the 218 for rgb is the grey color of the background
         arcView.addSeries(new SeriesItem.Builder(Color.argb(255, 218, 218, 218))
                 .setRange(0, 100, 100)
                 .setInitialVisibility(false)
@@ -31,16 +33,41 @@ public class MainActivity extends Activity {
                 .setLineWidth(32f)
                 .build();
 
-        int series1Index = arcView.addSeries(seriesItem1);
+        final int series1Index = arcView.addSeries(seriesItem1);
 
         // Add events to animate the data series
         arcView.addEvent(new DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
-                .setDelay(1000)
-                .setDuration(2000)
+                //.setDelay(1)
+                .setDuration(1000)  // it's the time required to display the grey ring. This includes the animation of the ring expanding to it's actual size
                 .build());
 
-        arcView.addEvent(new DecoEvent.Builder(25).setIndex(series1Index).setDelay(4000).build());
-        arcView.addEvent(new DecoEvent.Builder(100).setIndex(series1Index).setDelay(8000).build());
-        arcView.addEvent(new DecoEvent.Builder(10).setIndex(series1Index).setDelay(12000).build());
+
+        // setDelay implies the amount of milliseconds before the event/animation is triggered
+        arcView.addEvent(new DecoEvent.Builder(25).setIndex(series1Index).setDelay(8000).build());
+        //arcView.addEvent(new DecoEvent.Builder(100).setIndex(series1Index).setDelay(8000).build());
+        //arcView.addEvent(new DecoEvent.Builder(10).setIndex(series1Index).setDelay(12000).build());
+
+        final int[] progress = {0};
+        final Handler handler = new Handler();
+        handler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        arcView.addEvent(new DecoEvent.Builder(progress[0]).setIndex(series1Index).setDelay(1000).build());
+                        progress[0]++;
+                        if (progress[0] <= 100 )
+                            handler.postDelayed(this, 1000);
+                    }
+                }
+        );
+
+        /*for ( int i = 0; i <= 100; i++ ) {
+            arcView.addEvent(new DecoEvent.Builder(i).setIndex(series1Index).setDelay(4000).build());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }*/
     }
 }
